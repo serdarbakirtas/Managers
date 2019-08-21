5fed0be05d141bb89586b2e1de0cd89daba0770e
Hasan Bakirtas <serdarbakirtas@gmail.com>
2019-08-21 13:15:15 +0300

    Create LanguageManager

---
+++ LanguageManager.swift
@@ -0,0 +1,21 @@
+//
+//  LanguageManager.swift
+//  AlnoorTV
+//
+//  Created by Hasan Bakirtas on 4.07.2019.
+//  Copyright © 2019 Hasan Bakirtas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LanguageManager {
+    
+    static let shared = LanguageManager()
+    
+    private init() {}
+    
+    func launch(_ window: UIWindow?) {
+
+    }
+}
