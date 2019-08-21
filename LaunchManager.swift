63f85dc5cb9b63e4ebee6081dd355bd0c7b68002
Hasan Bakirtas <serdarbakirtas@gmail.com>
2019-08-21 13:13:38 +0300

    Create LaunchManager

---
+++ LaunchManager.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+
+class LaunchManager {
+    
+    static let shared = LaunchManager()
+    
+    private init() {}
+ 
+    func launch(_ application: UIApplication, launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
+        
+//        FirebaseHelper.configure()
+//        GoogleHelper.configure()
+        initializeLogger()
+    }
+    
+    private func initializeLogger() {
+        
+        // Debug: Log everything only to console, no logging to SwiftyBeaver Platform
+        // Change the below configuration for a more filtered console logging
+    }
+}
